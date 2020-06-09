7f8451c85b969d89f911e9b029c5283fc8d15cbe
Ben Hutchison <1417794+Aldaviva@users.noreply.github.com>
2020-06-08 18:51:45 -0700

    Initial commit, working

---
+++ KeePassTrayIconLockState/KeePassTrayIconLockStateExt.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+using System.Drawing;
+using System.Threading.Tasks;
+using KeePass.App;
+using KeePass.Plugins;
+using KeePass.UI;
+using KoKo.Property;
+
+namespace KeePassTrayIconLockState {
+
+    // ReSharper disable once UnusedType.Global
+    public class KeePassTrayIconLockStateExt: Plugin {
+
+        private IPluginHost keePassHost = null!;
+
+        private readonly StoredProperty<bool> isFileOpen = new StoredProperty<bool>(false);
+
+        public override bool Initialize(IPluginHost host) {
+            keePassHost = host;
+
+            keePassHost.MainWindow.FileOpened += delegate { isFileOpen.Value = true; };
+            keePassHost.MainWindow.FileClosed += delegate { isFileOpen.Value = false; };
+
+            isFileOpen.PropertyChanged += delegate { renderTrayIcon(); };
+
+            Task.Delay(20).ContinueWith(task => renderTrayIcon()); // Give KeePass time to stop setting its own icon
+
+            return true;
+        }
+
+        private void renderTrayIcon() {
+            AppIconType appIconType = isFileOpen.Value ? AppIconType.QuadNormal : AppIconType.QuadLocked;
+            Icon appIcon = AppIcons.Get(appIconType, UIUtil.GetSmallIconSize(), Color.Blue);
+            keePassHost.MainWindow.MainNotifyIcon.Icon = appIcon;
+        }
+
+    }
+
+}
---
+++ KeePassTrayIconLockState/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KeePassTrayIconLockState")]
+[assembly: AssemblyDescription("Show the locked symbol in the KeePass tray icon when the password database is closed or being decrypted.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ben Hutchison")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright © Ben Hutchison 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e980abdc-e56d-4e9c-a322-afbe9d9092b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
